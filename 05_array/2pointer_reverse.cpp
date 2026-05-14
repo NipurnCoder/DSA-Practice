bbcdfacad683b86a1815d7bf7b85fcd74ab08b87
NipurnCoder <nipurn.builds@gmail.com>
2026-05-14 18:30:08 +0530

    Revise 2-Pointer Approach today

--- 05_array/2pointer_reverse.cpp
+++ 05_array/2pointer_reverse.cpp
@@ -1,31 +1,32 @@
 #include<iostream>
 using namespace std;
 
-void reverse(int arr[], int size)
+// Revise this approach today
+
+void reverse(int arr[], int size) //arr of size 6 or arr[6]
 {
-    int start=0, end=size-1;
+    int start=0, end=size-1; //Starting Pointer, Ending Pointer in Array 
 
     while(start<end)
     {
-        swap(arr[start],arr[end]);
-        start++;
-        end--;
+        swap(arr[start],arr[end]); //use Swap Function to reverse it's elements
+        start++;    //update start pointer
+        end--;      //same for end pointer
     }
 }
 
 int main()
 {
-    int arr[] = {4,5,6,7,8,9};
+    int arr[] = {4,5,6,7,8,9}; //elements in array
     int size=6;
     cout<<"Reverse array - "<<endl;
 
-    reverse(arr,size);
+    reverse(arr,size); //Funtion Call
     
-        for(int i=0;i<size;i++)
+        for(int i=0;i<size;i++) //loop to print Array's elements in reverse order
         {
             cout<<arr[i]<<" ";
         }
     
-
     return 0;
 }
