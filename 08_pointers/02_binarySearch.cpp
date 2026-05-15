4bdeddac8b00f4d82040bc4dfb2bb60e4b76b564
NipurnCoder <nipurn.builds@gmail.com>
2026-05-15 17:43:06 +0530

    Revise : Binary Search for LeetCode : 33, 153

--- 08_pointers/02_binarySearch.cpp
+++ 08_pointers/02_binarySearch.cpp
@@ -2,19 +2,22 @@
 #include<vector>
 using namespace std;
 
+//Revise Binary Search today
+
 int binarySearch(vector<int> arr, int tar){
-    int st=0, end=arr.size()-1;
+    int st=0, end=arr.size()-1;     //Two pointers approach
 
     while(st<=end){
         //int mid = (st+end)/2; //This might overflow
         int mid = st+(end-st)/2;
-        cout<<"mid is: "<<arr[mid]<<endl; 0.
+        cout<<"mid is: "<<arr[mid]<<endl; 
         
+        //Array is already sorted in ascending order
 
-        if(tar > arr[mid]){
+        if(tar > arr[mid]){     //search in left 
             st = mid+1; //2nd half
         }
-        else if(tar < arr[mid]){
+        else if(tar < arr[mid]){    //search in right
             end = mid-1; //1st half
         }
         else{
@@ -39,6 +42,8 @@ int main(){
     cin>>tar;
 
     cout<<"Target value "<< tar<<" Found at index = "<<binarySearch(arr, tar)<<endl;
+
+    return 0;
 }
 
  
