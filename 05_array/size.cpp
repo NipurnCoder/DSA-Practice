d58e1b63187c16bdfbbbfc2e2f83dbaf1deaaad5
NipurnCoder <nipurn.builds@gmail.com>
2026-05-17 16:25:13 +0530

    Revise : Input/output of array, size calculation

--- 05_array/size.cpp
+++ 05_array/size.cpp
@@ -1,36 +1,40 @@
 #include<iostream>
 using namespace std;
 
+//Revise Today 17/5
+
 int main()
 {
-    int n;
-    cout<<"Enter size of Array : ";
-    cin>>n;
-    int size=n;
+    //int n;
+    int size;
+    cout<<"Enter size of Array : "; //Take input of arr's size
+    cin>>size;
+
+    //int size=n;
     int num[size];
 
     cout<<"Enter elements of array : ";
 
-    for(int i=0;i<size;i++)
+    for(int i=0;i<size;i++)     //Taking the elements of arr
     {
         cin>>num[i];
     }
 
     cout<<"Array is -- ";
 
-    for(int i=0;i<size;i++)
+    for(int i=0;i<size;i++)     //Print the elements
     {
         cout<<num[i]<<" ";
     }
     
-// int size = sizeof(num)/sizeof(int);
-//     int num[size];
+// int size = sizeof(num)/sizeof(int);      Calculate size of arr by mathematics
+//     int num[size];                       1 int = 4 bytes
 //      cout<<size;
+
 cout<<endl;
 int sz=sizeof(num)/sizeof(int);
+
 cout<<"Size of array is : "<<sz<<endl;
-
-
 
 return 0;
 }
