e926b08bfce064765e3fb4e04e84c91f03a4e6c3
NipurnCoder <nipurn.builds@gmail.com>
2026-05-19 17:21:46 +0530

    Revise : Unique number in arr

--- 05_array/unique.cpp
+++ 05_array/unique.cpp
@@ -1,20 +1,29 @@
 #include<iostream>
 using namespace std;
 
+//Revise today 19/05
+
+//function
 int unique_num (int arr[], int size)
 {
-    cout<<"Unique elements are "<<endl;
+    cout<<"Unique elements are : ";
+
+    //loop 1
     for(int i=0;i<size;i++)
     {
+        //initialise count
         int count=0;
+        //loop 2
         for(int j=0;j<size;j++)
         {
+            //if val is same
             if(arr[i]==arr[j])
             {
-                count++;
+                count++; //increase count
             }
         }
 
+        //only one element same
         if(count==1)
         {
             cout<<arr[i]<<" ";
@@ -38,6 +47,9 @@ int main()
     int arr[] = {1,2,3,4,5,1,2};
     int size=7;
 
+    //Function call
     unique_num(arr,size);
+
+    return 0;
 
 }
