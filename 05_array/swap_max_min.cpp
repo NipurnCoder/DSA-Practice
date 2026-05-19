5ab4c45ecc52c4d6e8e0f145c23f0b21ea7bff21
NipurnCoder <nipurn.builds@gmail.com>
2026-05-19 17:08:26 +0530

    Revise : Swap the min - max value in array

--- 05_array/swap_max_min.cpp
+++ 05_array/swap_max_min.cpp
@@ -1,21 +1,29 @@
 #include<iostream>
-#include<climits>
+#include<climits> 
+//climits to use max and min
 using namespace std;
 
-int swap_min_max(int arr[], int size)
+//Revise todat 19/05
+
+
+//Funtion 
+int swap_min_max(int arr[], int size) 
 {
-    int a=INT_MIN, num;
+    //initialization with min, max
+    int a=INT_MIN, num; 
     int b=INT_MAX, min;
 
     int num_index=-1, min_index=-1;
 
     for(int i=0;i<size;i++)
     {
-        if(arr[i]>a)
+        //always true
+        if(arr[i]>a) 
         {
             a=arr[i];
             num=arr[i];
             num_index=i;
+            //max found
         }
 
         if(arr[i]<b)
@@ -23,11 +31,14 @@ int swap_min_max(int arr[], int size)
             b=arr[i];
             min=arr[i];
             min_index=i;
+            //min found
         }
     }
+
     cout<<"Max number : "<<num<<endl;
     cout<<"Min number : "<<min<<endl;
 
+    //swaping with 3 variables
     int temp=arr[num_index];
     arr[num_index]=arr[min_index];
     arr[min_index]=temp;
@@ -45,16 +56,21 @@ int swap_min_max(int arr[], int size)
 
 int main()
 {
-    int arr[]={22,-45,67,-29,87,99,10};
-    int size=7;
+    //int arr[]={22,-45,67,-29,87,99,10};
+    int arr[]={4,5,-2,1,3,-7};
+    int size=6;
 
     cout<<"Original array "<<endl;
 
+    //print arr
     for(int i=0;i<size;i++)
     {
         cout<<arr[i]<<" ";
     }
     cout<<endl;
 
+    //function call
     swap_min_max(arr,size);
+
+    return 0;
 }
