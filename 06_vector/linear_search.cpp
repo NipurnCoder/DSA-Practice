efbfc18e82322062e53f455cd100fcd3fa48a8d4
NipurnCoder <nipurn.builds@gmail.com>
2026-05-20 19:06:12 +0530

    Revise : Vector Linear Approach

--- 06_vector/linear_search.cpp
+++ 06_vector/linear_search.cpp
@@ -2,12 +2,17 @@
 #include<vector>
 using namespace std;
 
+//Revise today 02/05
+
+//function store vec, tar
+
 int linear_search(vector<int>vec, int target)
 {
    // int ans=0;
     for(int i=0;i<vec.size();i++)
 
     {
+        //if target is find return idx
         if(vec[i]==target)
            // ans=vec[i];
             return i;
@@ -20,8 +25,10 @@ int main()
     vector<int>vec = {45,67,89,90,23};
     int target=90 ;
 
+    //function call
     int index=linear_search(vec,target);
 
+    //loop for print idx
     if(index!= -1)
     {
         cout<<"Element found at index "<<index<<endl;
@@ -29,4 +36,6 @@ int main()
     else{
         cout<<"ELement NOT Found !! "<<index<<endl;
     }
+
+    return 0;
 }
