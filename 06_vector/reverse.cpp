ee05e59fb8ff6fb921cf2ce3fe26da0cf772d9a2
NipurnCoder <nipurn.builds@gmail.com>
2026-05-21 22:42:00 +0530

    Revise : Reverse the vector using Swap Function and 2 pointer approach

--- 06_vector/reverse.cpp
+++ 06_vector/reverse.cpp
@@ -2,13 +2,20 @@
 #include<vector>
 using namespace std;
 
+//Revise today 21/05
+
+//function to revese
 int reverse_vec(vector<int>&vec)
 {
     int start=0, end=vec.size()-1;  //two pointer approch
+    //2 ptr st/end
 
+    //base condition
     while(start<end)
     {
+        //in-build swap fn
         swap(vec[start], vec[end]);
+        //update st/end
         start++;
         end--;
     }
@@ -17,15 +24,19 @@ int reverse_vec(vector<int>&vec)
 int main()
 {
     vector<int> vec = {34,56,78,98,12};
+    //original vector
     for(int i: vec){
         cout<<i<<" ";
     }
     cout<<endl<<"Reverse is \n";
 
+    //after reverse
     reverse_vec(vec);
     for(int i : vec)
     {
         cout<<i<<" ";
     }
     cout<<endl;
+
+    return 0;
 }
