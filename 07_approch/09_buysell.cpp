c2266e763c9feee7c6bb03c5d4eeeb2b88de33be
NipurnCoder <nipurn.builds@gmail.com>
2026-05-22 13:45:01 +0530

    Revision Day 09 : Best time to buy and sell stocks|LeetCode : 121 solved

--- 07_approch/09_buysell.cpp
+++ 07_approch/09_buysell.cpp
@@ -2,19 +2,30 @@
 #include<vector>
 using namespace std;
 
+//Revision Day 09
+//22-05-2026
 
-    int stock (vector<int> price){
-    int max_profit=0;
-    int best_buy=price[0];
+//Function to find max profit
+int stock (vector<int> price){      //LeetCode : 121 Best Time To Buy and Sell Stocks
+
+    //initialize maxProfit 
+    int max_profit = 0;
+    //Intitialize bestBuy with vecPrice val at idx=0
+    int best_buy = price[0];
+
 
     for(int i=1;i<price.size();i++){
+        
+        //curr day val should be less than future val
         if(price[i]>best_buy){
+            //find max profit future - curr
             max_profit = max(max_profit, price[i]-best_buy);
         }
+        //cal best day
         best_buy = min(best_buy, price[i]);
     }
     return max_profit;
- }
+}
 
 
 int main(){
