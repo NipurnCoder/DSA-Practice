3be32a3ed7c7e70f7013c15ba3a6a08b08100300
NipurnCoder <nipurn.builds@gmail.com>
2026-05-28 23:20:45 +0530

    Update code

--- 07_approch/majority_moores.cpp
+++ 07_approch/majority_moores.cpp
@@ -9,6 +9,11 @@ using namespace std;
     
     Approach : Boyer Moore Voting Algorithm
 
+    Finds element whose freq is more than half of the array's size
+    1st increase freq by 1 of element 
+    2nd decrease freq if  element not same
+    finally, freq left to those element who appear max after freq--
+
     Time : O(n)
     Space : O(n)
 
@@ -18,26 +23,30 @@ int majority(vector<int> nums){
     int n = nums.size();
     int freq=0, ans=0;
 
-    for(int i=0;i<n;i++){
+    for(int i=0; i<n; i++){
         if(freq==0){
             ans = nums[i];
-           // freq=1;
+
         }
+
         if(ans==nums[i]){
             freq++;
         }
         else{
             freq--;
         }    
-            
-        return ans;
+        cout<<freq<<" ";
     }
+
+    return ans;
 }
 
 int main(){
 
     //vector<int>nums = {2,3,4,5,5,5,5,4,3,4,2,1};
-    vector<int> nums = {3,3,3,4,2,2,5,5,4,5,3,3,3,3,3};
+    //vector<int> nums = {1,2,2,1,1,2,2};
+    vector<int> nums  = {3,3,3,4,2,2,5,5,4,5,5,5,5,5,3,3};
+
     int m = majority(nums);
 
     cout<<"Majority element is : "<<m<<endl;
