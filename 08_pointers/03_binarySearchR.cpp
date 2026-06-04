f5a0641cdb9874d0e0cdab4fdab4b13241d464b8
NipurnCoder <nipurn.builds@gmail.com>
2026-06-04 22:56:34 +0530

    Revision Day 21 : Binary Search by Recurrsion | LC-704 : Binary Search

--- 08_pointers/03_binarySearchR.cpp
+++ 08_pointers/03_binarySearchR.cpp
@@ -2,16 +2,49 @@
 #include<vector>
 using namespace std;
 
+//Revision Day 21 [4/6/26]
+
+/*
+    LeetCode-704 :  Binary Search  
+
+    Concept - Binary Seaech by Reccursion
+
+    Time - O(log n)
+    Space - O(log n)
+
+    recBinarySearch(arr, 9, 0, 5)
+            |
+          mid=2
+            |
+        9 > 3
+            |
+            v
+ recBinarySearch(arr, 9, 3, 5)
+            |
+          mid=4
+            |
+        arr[4]=9
+            |
+         return 4
+
+*/
+
+//Function
 int recBinarySearch(vector<int>& arr, int tar, int st, int end){
+
     if(st<=end){
+        //Calc mid
         int mid = st + (end-st)/2;
 
+        //Right search
         if(tar>arr[mid]){
             return recBinarySearch(arr, tar, mid+1, end);
         }
+        //Left search
         else if(tar <arr[mid]){
             return recBinarySearch(arr, tar, st, mid-1);        //recurcive
         }
+        //target == mid
         else {
             return mid;
         }
@@ -20,12 +53,15 @@ int recBinarySearch(vector<int>& arr, int tar, int st, int end){
 }
 
 int main(){
+    //sorted vector ASCE
     vector<int> arr = {-1, 0, 3, 5, 9, 12};
-    int tar =3;
+
+    int tar =9;
     int st=0;
     int n = arr.size();
     int end=n-1;
 
-    cout<<recBinarySearch(arr,tar,st,end)<<endl;
+    cout<<tar<<" Found at idx : "<<recBinarySearch(arr,tar,st,end)<<endl;
+
     return 0;
 }
