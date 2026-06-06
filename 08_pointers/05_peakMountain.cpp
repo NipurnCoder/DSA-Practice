dc6f65c041594def3cf3f7c3d53842e06b6f1557
NipurnCoder <nipurn.builds@gmail.com>
2026-06-06 18:34:16 +0530

    Revision : Linear Search & Binary Search to find Peak Element | LC-162, 852

--- 08_pointers/05_peakMountain.cpp
+++ 08_pointers/05_peakMountain.cpp
@@ -2,11 +2,37 @@
 #include <vector>
 using namespace std;
 
+//Revision Day 23 [6/6/26]
+
+/*
+    LeetCode 162 : Find Peak Element
+    1st Approach: Linear Search
+    Idea:
+    - Traverse the array from index 1 to n-2.
+    - Check if the current element is greater than both neighbors.
+    - The first element satisfying this condition is the peak.
+
+    Time - O(n)
+    Space - O(1)
+    
+
+    LeeCode 852 : Peak Index in a Mountain Array
+    2nd Appraoch: Binary Search
+    Idea:
+    - If mid is on the increasing slope, move right.
+    - If mid is on the decreasing slope, move left.
+    - The point where arr[mid] is greater than both neighbors
+      is the peak element.
+
+    Time - O(log n)
+    Space - O(1)
+*/
+
 int linearSearch (vector<int>& arr){
 
     int n = arr.size();
 
-    for(int i=1;i<=n-1;i++){
+    for(int i=1; i<=n-1; i++){
         if(arr[i-1]<arr[i] && arr[i]>arr[i+1]){
             return arr[i];
         }
