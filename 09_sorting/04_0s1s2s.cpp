9ec361b122f08671e3e14dfb1e3c99cb6cd1ab9f
NipurnCoder <nipurn.builds@gmail.com>
2026-06-15 16:35:56 +0530

    Revision : Sort an Array contains 0s, 1s, 2s | Approach : (Brute Force, Counting Method)

--- 09_sorting/04_0s1s2s.cpp
+++ 09_sorting/04_0s1s2s.cpp
@@ -3,34 +3,60 @@
 #include<algorithm>
 using namespace std;
 
+//Revision Day 31
+
+/*
+    Sort an array containing only 0s, 1s, and 2s.
+
+    Approach 1 (Brute Force): 
+    - Use sort() function.
+    - Time Complexity: O(n log n)
+    - Space Complexity: O(1)
+
+    Approach 2 (Counting Method):
+    - Count frequency of 0s, 1s, and 2s.
+    - Overwrite array using counts.
+    - Time Complexity: O(n)
+    - Space Complexity: O(1)
+*/
+
 void bruteForce(vector<int> &num){  //TC=O(nlogn) SC=O(1)
+
     sort(num.begin(), num.end());
 }
 
 void optimised(int arr[], int n){ //optimied approach TC=O(n)
 
-    int count0=0, count1=0, count2=0;
+    int count0 = 0, count1 = 0, count2 = 0;
 
-    for(int i=0;i<n;i++){
-        if(arr[i]== 0) count0++;
-        else if(arr[i]== 1) count1++;
+    for(int i=0; i<n; i++){
+
+        //Count the Value's freq
+        if(arr[i] == 0) count0++;
+        else if(arr[i] == 1) count1++;
         else count2++;
     }
 
     int idx = 0;
 
-    for(int i=0;i<count0;i++){
-        arr[idx++] = 0;
+    //assign in array as count
+    for(int i=0; i<count0; i++){
+        arr[idx] = 0;
+        idx++;
+        //arr[idx++] = 0;
     }
-    for(int i=0;i<count1;i++){
+
+    for(int i=0; i<count1; i++){
         arr[idx++] = 1;
     }
-    for(int i=0;i<count2;i++){
+
+    for(int i=0; i<count2; i++){
         arr[idx++] = 2;
     }
 }
 
 void printVector(vector<int> &num){
+
     for(int i : num){
         cout<<i<<" ";
     }
@@ -38,11 +64,13 @@ void printVector(vector<int> &num){
 }
 
 void printArray(int arr[], int n){
-    for(int i=0;i<n;i++){
+
+    for(int i=0; i<n; i++){
         cout<<arr[i]<<" ";
     }
     cout<<endl;
 }
+
 int main(){
 
     vector<int> num = {2,0,2,1,1,0,1,2,0,0};
