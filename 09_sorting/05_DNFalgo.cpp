06d538370055552e3736f1e417b83340649ec87f
NipurnCoder <nipurn.builds@gmail.com>
2026-06-16 16:34:54 +0530

    Revision : Dutch National Flag (low, mid, high) | LC 75 : Sort Colors

--- 09_sorting/05_DNFalgo.cpp
+++ 09_sorting/05_DNFalgo.cpp
@@ -1,15 +1,46 @@
 #include<iostream>
 using namespace std;
 
+//Revision Day 32
+
+/*
+    LeetCode 75 : Sort Colors
+
+    Dutch National Flag Algorithm
+
+        Idea:
+       - Sort array containing only 0s, 1s and 2s
+       - low -> boundary of 0s
+       - mid -> current element
+       - high -> boundary of 2s
+
+        0 ... low-1      -> all 0s
+        low ... mid-1    -> all 1s
+        mid ... high     -> unsorted
+        high+1 ... n-1   -> all 2s
+
+       Time Complexity = O(n)
+       Space complexity = O(1)
+       
+*/
+
 void dutchNationalFlage(int arr[], int n){ //O(n)
+
     int low = 0, mid = 0, high = n-1;
+
     while(mid <= high){
+
+        //Place 0 in the left region.
         if(arr[mid] == 0){
             swap(arr[low], arr[mid]);
             mid++; low++;
-        } else if(arr[mid] == 1){
+
+        }//1 is already in the correct middle region.
+        else if(arr[mid] == 1){
             mid ++;
-        } else{
+
+        }//Place 2 in the right region.
+        else{ //mid == 2
             swap(arr[high], arr[mid]);
             high--;
         }
@@ -17,7 +48,8 @@ void dutchNationalFlage(int arr[], int n){ //O(n)
 }
 
 void print(int arr[], int n){
-    for(int i=0;i<n;i++){
+
+    for(int i=0; i<n; i++){
         cout<<arr[i]<<" ";
     }
     cout<<endl;
