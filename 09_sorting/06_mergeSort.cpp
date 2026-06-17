f1295b6a5fa69d528d58e8086cb5afc976d34b4a
NipurnCoder <nipurn.builds@gmail.com>
2026-06-17 23:34:24 +0530

    Revision : Merge Sort (3 ptr) | LC 912, 88 : Merge Sorted Array

--- 09_sorting/06_mergeSort.cpp
+++ 09_sorting/06_mergeSort.cpp
@@ -2,10 +2,34 @@
 #include<vector>
 using namespace std;
 
+//Revision Day 33 [17/6/26]
+
+/*
+    LeetCode 912, 88 :  Merge Sorted Array 
+
+    Merge Sorted Array
+
+    Approach :
+          
+        Use three pointers:
+        i -> last valid element of A
+        j -> last element of B
+        idx -> last position of A
+
+        Compare from the back and place the larger element.
+        If B still has elements left, copy them into A.
+
+        Time Complexity = O(m+n)
+        Space Complexity = O(1)
+
+*/
+
 void merge(vector<int> &A, int m, vector<int> &B, int n){  //O(1) SC    O(m+n) TC
+
     int idx = m+n-1, i = m-1, j = n-1;
 
-    while(i>=0 && j>=0){
+    while(i >= 0 && j >= 0){
+
         if(A[i] >= B[j]){
             A[idx--] = A[i--]; //A[idx] = b[j];    
         } else{                //idx--; j--;
@@ -13,7 +37,8 @@ void merge(vector<int> &A, int m, vector<int> &B, int n){  //O(1) SC    O(m+n) T
         }
     }
 
-    while(j>=0){
+    //if j remains
+    while(j >= 0){
         A[idx--] = B[j--];
     }
 }
