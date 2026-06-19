36fcc0e9111fedae4abe7a8656ec3ae6f77524fc
NipurnCoder <nipurn.builds@gmail.com>
2026-06-19 18:05:18 +0530

    Revision : String's Functions (strlen, getline) | LC 58 : Length of last word

--- 10_string/01_getline.cpp
+++ 10_string/01_getline.cpp
@@ -2,20 +2,42 @@
 #include<string>
 using namespace std;
 
+//Revision Day 35
+
+/*
+    LeetCode 58 : Length of last word
+    
+    Character Array & String Basics
+
+    strlen for char array -> traverse until '\0'
+    string.length() -> O(1)
+    getline(cin, s) -> input with spaces
+    cin.getline(arr, size, delimeter) -> char array input
+
+    Time Complexity = O(n) manualy length
+    Space Complexity = O(1)
+
+*/
+
 void arrStr(){
+
     char str[] = "Hello Knowledge";
     int len = 0;
 
-    for(int i=0; i<str[i] != 0; i++){ //for(int i=0;str[i] !='/o';i++)
+    //calculate length 
+    for(int i=0; str[i] != 0; i++){ //for(int i=0;str[i] !='/o';i++)
         len++;
     }
     cout<< "length of string : "<<len<<endl;
 
+    //Claculate by inbuilt-function length
     string s1 = "yo broo ";
     cout<<s1.length()<<endl;
     
+    //take input of string with space by using getline function
     string s2 ;
     cout<<"enter "<<endl;
+
     getline(cin,s2);
     cout<<s2<<endl;
     
@@ -30,6 +52,8 @@ int main(){
     // cin.getline(str, 100, '.');
 
     // cout<<"Output is : "<<str<<endl;
+
     arrStr();
+
     return 0;
 }
