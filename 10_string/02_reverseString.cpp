dc3b9ead5969c19ac44b80c9c291ec0c6a1c4e7f
NipurnCoder <nipurn.builds@gmail.com>
2026-06-20 12:32:13 +0530

    Revision : Reverse String (2-ptr, STL reverse) | LC 344 : Reverse the String

--- 10_string/02_reverseString.cpp
+++ 10_string/02_reverseString.cpp
@@ -4,13 +4,36 @@
 #include<algorithm>
 using namespace std;
 
+// Revision Day 36
+
+/*
+    LeetCode 344 - Reverse String
+    
+    Reverse String
+
+    Approach 1:
+    - Use two pointers (start, end)
+    - Swap characters until start < end
+
+    Approach 2:
+    - Use STL reverse() function
+
+    Time Complexity : O(n)
+    Space Complexity : O(1)
+
+*/
+
 void reverseString(vector<char>& str){
+
     int st = 0, end = str.size()-1;
 
+    //reverse string in vector form
     while(st<end){
+        //using swap function
         swap(str[st++],str[end--]);
     }
 
+    //print the string
     for(char i : str){
         cout<<i<<" ";
     }
@@ -23,9 +46,12 @@ int main(){
     reverseString(str);
 
     string s = "hello";
-
+    
+    //reverse string with reverse function
     reverse(s.begin(), s.end());
+
     cout<<s<<endl;
+
     return 0;
 }
 
