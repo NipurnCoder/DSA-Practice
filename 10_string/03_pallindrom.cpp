8cefa708628123fc6a730493f4df463fa84b1aee
NipurnCoder <nipurn.builds@gmail.com>
2026-06-21 13:40:58 +0530

    Revision : Basic Palindrome (2-ptr, STL function) | LC 2108 : Find First Palindrome String in the Array

--- 10_string/03_pallindrom.cpp
+++ 10_string/03_pallindrom.cpp
@@ -4,27 +4,51 @@
 #include<algorithm>
 using namespace std;
 
+//Revision Day 37 [21/6/26]
+
+/*
+    LeetCode 2108 : Find First Palindromic String in the Array
+
+    (Basic Palindrome Check)
+
+    Approach:
+        - Use two pointers (start and end).
+        - Compare characters from both ends.
+        - If any pair doesn't match, it's not a palindrome.
+        - Otherwise move both pointers inward.
+        - If all pairs match, it's a palindrome.
+
+    Time Complexity : O(n)
+    Space Complexity : O(1)
+*/
+
 void palindrom(vector<char>&str){
+
     int st = 0, end = str.size()-1;
-    while(st<end){
-        if(str[st] != str[end]){
-            cout<<"Unvalid Palindrom !! "<<endl;
-            return;
-        }
-        st++;
-        end--;
-    }
-    cout<<"Valid Palindrom "<<endl;
+
     // while(st<end){
-    //     if(str[st++] == str[end--]){
-    //         cout<<"Valid Palindrom "<<endl;
+
+    //     if(str[st] != str[end]){
+    //         cout<<"Unvalid Palindrom !! "<<endl;
+    //         return;
     //     }
-    //     else {
-    //         cout<<"Unvalid palindrome !!"<<endl;
-    //     }
+    //     st++;
+    //     end--;
     // }
+    // cout<<"Valid Palindrom "<<endl;
+
+    //2nd way
+
+    while(st<end){
+        if(str[st++] != str[end--]){
+            cout<<"UnValid Palindrom "<<endl;
+             return;
+        }
+    }
+      cout<<"Valid palindrome !! "<<endl;
 }
-void palStr(const string& s){
+
+void palindromStr(const string& s){
         int st = 0, end = s.length()-1;
 
         while(st<end){
@@ -43,15 +67,18 @@ int main(){
     vector<char>str = {'r','a','c','e','c','a','r'};
     palindrom(str);
 
-     string s = "RACeCAR";
-     palStr(s);
+    string s = "RACeCAR";
+    //palindromStr(s);
 
-    // if(s.begin() == s.end()){
-    //     cout<<"This is a Plindrome "<<endl;
-    // }
-    // else{
-    //     cout<<"Not! a Palindrom"<<endl;
-    // }
+    //Another way
+
+    string temp = s;
+    reverse(temp.begin(), temp.end());
+
+    if(temp == s)
+        cout << "Palindrome";
+    else
+        cout << "Not Palindrome";
 
     return 0;
 }
