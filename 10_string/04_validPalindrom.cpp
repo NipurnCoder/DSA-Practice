9f95d45cb5b4eebce9fa86dcf7e6bbc5bb905872
NipurnCoder <nipurn.builds@gmail.com>
2026-06-22 20:07:55 +0530

    Revision : Valid Palindrome (alphaNumeric) | LC 125 : Valid Palindrome

--- 10_string/04_validPalindrom.cpp
+++ 10_string/04_validPalindrom.cpp
@@ -3,7 +3,24 @@
 #include<vector>
 using namespace std;
 
+//Revision Day 38
+
+/*
+    LeetCode 125 : Valid Palindrome
+
+    Approach:
+    - Use two pointers.
+    - Skip non-alphanumeric characters.
+    - Compare characters ignoring case.
+    - If all characters match, return true.
+
+    Time Complexity : O(n)
+    Space Complexity : O(1)
+*/
+
 bool isAlpha(char ch){
+
+    //Allow numbers and alphabets
     if((ch >= '0' && ch <= '9') || (tolower(ch) >= 'a' && tolower(ch) <= 'z')){
         return true;
     }
@@ -11,25 +28,29 @@ bool isAlpha(char ch){
 }
 
 bool isPalindrome(string s){
+
     int st = 0, end = s.length()-1;
 
-    while(st<end){
-        if(isAlpha(s[st])){  
+    while(st < end){ 
+
+        if(!isAlpha(s[st])){  
             st++; continue;
         }
-        if(isAlpha(s[end])){
+        if(!isAlpha(s[end])){
             end--; continue;
         }
         if(tolower(s[st]) != tolower(s[end])){
             return false;
         }
-    } 
+        st++;
+        end--;
+    }           
     return true;
 }
 
 int main(){
 
-    string s = "A/Bb/a";
+    string s = "A/B//]sgSb?/a";
     isPalindrome(s);
 
     if(isPalindrome(s)){
