385edbae4501b6fd212826cffc1b3725b44067d1
NipurnCoder <nipurn.builds@gmail.com>
2026-06-23 13:30:19 +0530

    Revision : Remove Occurrences (erase, find) | LC 1910 : Remove All Occurrences of a Substring

--- 10_string/05_removeOccurrence.cpp
+++ 10_string/05_removeOccurrence.cpp
@@ -2,7 +2,24 @@
 #include<string>
 using namespace std;
 
+// Revision Day 39
+
+/*
+    LeetCode 1910: Remove All Occurrences of a Substring
+    
+    Remove All Occurrences of a Substring
+
+    Approach:
+    - Find the substring using find().
+    - If found, erase it using erase().
+    - Repeat until substring is not present.
+
+    Time Complexity: O(n²) (Worst Case)
+    Space Complexity: O(1)
+*/
+
 void removeOccurence(string s , string part){
+    
     int st = 0, end = s.length()-1;
 
     while(s.length() > 0 && s.find(part) < s.length()){
