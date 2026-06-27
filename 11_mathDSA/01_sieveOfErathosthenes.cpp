9e8efe8deb0832bd52f425cfaa6e721a4ae480bf
NipurnCoder <nipurn.builds@gmail.com>
2026-06-27 18:47:37 +0530

    Revision Day 43 : mathDSA | Sieve of Eratosthenes (prime No.) | LC 204 : Count Primes

--- 11_mathDSA/01_sieveOfErathosthenes.cpp
+++ 11_mathDSA/01_sieveOfErathosthenes.cpp
@@ -2,6 +2,28 @@
 #include<vector>
 using namespace std;
 
+// Revision Day 43
+
+/*
+    LeetCode : 204 - Count Primes
+
+    Sieve of Eratosthenes
+
+    Approach:
+    1. Assume all numbers are prime.
+    2. Traverse from 2 to n-1.
+    3. If a number is prime, increment count.
+    4. Mark all its multiples as non-prime.
+
+    Optimization:
+    - Start marking from i*i instead of i*2.
+    - Multiples smaller than i*i are already processed.
+
+    Time Complexity  : O(n log log n)
+    Space Complexity : O(n)
+
+*/
+
 void countPrime(int n){
     vector<bool> isPrime(n+1, true);
         int count = 0;
@@ -10,7 +32,8 @@ void countPrime(int n){
             if(isPrime[i]){
                 count++;
 
-                for(int j=i*2; j<n; j=j+i){
+                
+                for(int j=i*2; j<n; j=j+i){ //int j=i*i; j<n; j=j+i
                     isPrime[j] = false;
                 }
             }
@@ -23,7 +46,7 @@ int main(){
     int n;
     cout<<"Enter the no. ";
     cin>>n;
-    cout<<endl<<"count of Prime numbers ";
+    cout<<"Count of Prime numbers ";
 
     countPrime(n);
 
