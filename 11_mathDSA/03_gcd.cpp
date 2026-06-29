120e72c92df4b2b7595bc9636e8923d16c0e4be2
NipurnCoder <nipurn.builds@gmail.com>
2026-06-30 01:09:33 +0530

    Revision : GCD & LCD (Euclidean Algorithm) | LC 1979, 1071, 365

--- 11_mathDSA/03_gcd.cpp
+++ 11_mathDSA/03_gcd.cpp
@@ -1,6 +1,33 @@
 #include<iostream>
 using namespace std;
 
+//Revision Day 45
+
+/*
+    LeetCode Practice:
+        LC 1979 - Find Greatest Common Divisor of Array
+        LC 1071 - Greatest Common Divisor of Strings
+        LC 365  - Water and Jug Problem
+
+    GCD & LCM using Euclidean Algorithm
+
+    Concepts Covered:
+        1. Iterative GCD (Euclidean Algorithm)
+        2. Recursive GCD
+        3. LCM using GCD
+
+    Formula:
+        GCD(a, b) = GCD(b, a % b)
+        LCM(a, b) = (a * b) / GCD(a, b)
+
+    Time Complexity:
+        gcd()    : O(log(min(a, b)))
+        gcdRec() : O(log(min(a, b)))
+        lcm()    : O(log(min(a, b)))
+
+*/
+
+//Greatest Common Divisor
 int gcd(int a, int b){
     while(a>0 && b>0){
         if(a > b){
@@ -13,11 +40,13 @@ int gcd(int a, int b){
     return a;
 }
 
+//By Recurrsion
 int gcdRec(int a, int b){
     if(b == 0) return a;
     return gcdRec(b, a % b);
 }
 
+//Lowest Common Multiple
 int lcm(int a, int b){      //LCM
     int gcd = gcdRec(a, b);
     return (a*b)/gcd;
