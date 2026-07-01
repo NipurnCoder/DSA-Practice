073bd19ff93ff69a58efc3df78d50e4dcf76b0a5
NipurnCoder <nipurn.builds@gmail.com>
2026-07-01 23:47:38 +0530

    Revision : 2D Array | LC 566 : Reshape the Matrix

--- 12_matrix/01_2D.cpp
+++ 12_matrix/01_2D.cpp
@@ -1,6 +1,29 @@
 #include<iostream>
 using namespace std;
 
+//Revision Day 47
+
+/*
+    LeetCode 566 : Reshape the Matrix
+    
+    Topic: 2D Arrays in C++
+
+    Concepts Covered:
+        1. Declaring a 2D array
+        2. Taking matrix input using nested loops
+        3. Traversing rows and columns
+        4. Printing a matrix
+        5. Understanding row-major order
+
+    Time Complexity:
+        Input  : O(row × col)
+        Output : O(row × col)
+
+    Space Complexity:
+        O(row × col)
+        
+*/
+
 int main(){
     // int arr[5] = {2,4,3,5,6};
     // int matrix[4][3] = {{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
@@ -9,16 +32,16 @@ int main(){
     int row = 4;
     int col = 3;
 
-    cout<<"Enter the elements "<<endl;
-    for(int i=0;i<row;i++){
-        for(int j=0;j<col;j++){
+    cout<<"Enter the elements -"<<endl;
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
             cin>>matrix[i][j];
         }
     }
 
-    cout<<"The matrix is "<<endl;
-    for(int i=0;i<row;i++){
-        for(int j=0;j<col;j++){
+    cout<<"The matrix is -"<<endl;
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
             cout<<matrix[i][j]<<" ";
         }
         cout<<endl;
