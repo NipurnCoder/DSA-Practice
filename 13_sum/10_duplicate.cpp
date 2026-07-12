c1f6108887393457aafd8bfe6da14c4fa7366596
NipurnCoder <nipurn.builds@gmail.com>
2026-07-12 13:37:34 +0530

    Revision Day 56 : Duplicate Number | LC 100 Days Complete [170+] | LC 217, 219, 1207

--- 13_sum/10_duplicate.cpp
+++ 13_sum/10_duplicate.cpp
@@ -3,19 +3,39 @@
 #include<unordered_set>
 using namespace std;
 
+//Revision Day 56
+
+/*
+    LeetCode  217, 219 :  Find Duplicate using Hash Set & II
+    LeetCode 1207 : Unique Number of Occurence
+
+    Topic : Duplicate Number
+
+    Store visited elements in unordered_set.
+    If an element is already present, it is the duplicate.
+
+    Time: O(n)
+    Space: O(n)
+*/
+
 int findDuplicate(vector<int>&arr){
+
     unordered_set<int> s;
+
     for(int val : arr){
         if(s.find(val) != s.end()){
             return val;
         }
         s.insert(val);
     }
+
     return -1;
 }
 
 int main(){
-    vector<int> arr = {3,1,3,4};
+
+    vector<int> arr = {3,1,9,9,4};
     cout<<"Duplicate no "<<findDuplicate(arr);
+
     return 0;
 }
