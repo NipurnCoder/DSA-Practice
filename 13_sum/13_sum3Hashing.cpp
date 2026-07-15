f5256356e822a7705153fbcff07eb75482d65121
NipurnCoder <nipurn.builds@gmail.com>
2026-07-15 14:07:23 +0530

    Revision Day 60 : 3 Sum (Hashing + Set) | LC : 16, 881, 611

--- 13_sum/13_sum3Hashing.cpp
+++ 13_sum/13_sum3Hashing.cpp
@@ -4,6 +4,24 @@
 #include<algorithm>
 using namespace std;
 
+//Revision Day 60
+
+/*
+    LeetCode 16, 881 : Boat to Save People
+    LeetCode 611 : Valid Triangle Number
+
+    Topic: 3Sum using Hashing + Set
+    
+    Idea:
+    1. Fix the first element.
+    2. Use a hash set to find the required third element for each second element.
+    3. Sort each triplet before inserting into a set to avoid duplicates.
+    4. Return all unique triplets whose sum is 0.
+
+    Time Complexity: O(n² log n)
+    Space Complexity: O(n)
+*/
+
 vector<vector<int>> sum3hash(vector<int>& num){
     int n = num.size();
 
@@ -13,22 +31,27 @@ vector<vector<int>> sum3hash(vector<int>& num){
         int tar = -num[i];
         set<int> s;
 
-        for(int j=i+1; j<n; j++){    //num[j]=second
+        for(int j = i+1; j<n; j++){    //num[j]=second
             int third = tar - num[j];   //third
 
             if(s.find(third) != s.end()){
                 vector<int> trip = {num[i], num[j], third};
+
                 sort(trip.begin(), trip.end());
                 uniqueTriplet.insert(trip);
             }
+
             s.insert(num[j]);
         }
     }
+
     vector<vector<int>> ans(uniqueTriplet.begin(), uniqueTriplet.end());
+
     return ans;
 }
 
 int main(){
+
     vector<int> num = {-1,0,1,2,-1,-4};
     vector<vector<int>> result = sum3hash(num);
 
@@ -38,6 +61,7 @@ int main(){
         }
         cout<<endl;
     }
+    
     return 0;
 }
 
