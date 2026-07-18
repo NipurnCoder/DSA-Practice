4d74b989f7f75a5403b9c03958501a631e51a09d
NipurnCoder <nipurn.builds@gmail.com>
2026-07-18 17:07:06 +0530

    Revision : Recursion | LC 412, 234

--- 14_recursion/01_basicRec.cpp
+++ 14_recursion/01_basicRec.cpp
@@ -1,7 +1,26 @@
 #include<iostream>
 using namespace std;
 
+//Revision Day 63 [18/7/2026]
+
+/*
+    LeetCode : 412 Fizz Buzz
+         234 : Palindrome Linked List
+    
+    Topic: Recursion
+    Problem: Print numbers in reverse order using recursion
+
+    Concepts Covered:
+    - Recursive function calls
+    - Base case to stop recursion
+    - Recursive case (self-call)
+    - Call stack behavior
+    - Time Complexity: O(n)
+    - Space Complexity: O(n) (recursion stack)
+*/
+
 void reverse(int n){
+
     if(n == 1){
         cout<<"1"<<endl;
         return;
@@ -12,6 +31,7 @@ void reverse(int n){
 }
 
 int main(){
+
     reverse(9);
     return 0;
 }
