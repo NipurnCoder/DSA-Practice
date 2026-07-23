4b6414ec4f6e8ed8318f1e210be4c048fac91ace
NipurnCoder <nipurn.builds@gmail.com>
2026-07-23 16:01:18 +0530

    Revision : Search in Sorted Array | LC : 278, 374

--- 14_recursion/06_binarySearch.cpp
+++ 14_recursion/06_binarySearch.cpp
@@ -2,9 +2,24 @@
 #include<vector>
 using namespace std;
 
+//Revision Day 68
+
+/*
+    LeetCode 278 : First Bad Version
+             374 : Guess Number Higher or Lower   
+
+    Topic: Recursion + Binary Search
+    Problem: Search in a Sorted Array
+
+    Time Complexity: O(log n)
+    Space Complexity: O(log n) (recursive call stack)
+*/
+
 int isFound(vector<int>& arr, int tar, int st, int end){
+
     if(st <= end){
-        int mid = st+(end-st)/2;
+        int mid = st + (end - st)/2;
+
         if(arr[mid] == tar){
             return mid;
         }
@@ -15,11 +30,14 @@ int isFound(vector<int>& arr, int tar, int st, int end){
             return isFound(arr, tar, st, mid-1);
         }
     }
+
     return -1;
 }
 
 int main(){
+
     vector<int> arr = {-1,0,3,5,9,12};
     cout<<isFound(arr, 9, 0, arr.size()-1);
+    
     return 0;
 }
