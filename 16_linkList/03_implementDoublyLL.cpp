862276eed463c68c5da295a9781457f90249a316
NipurnCoder <nipurn.builds@gmail.com>
2026-08-09 21:31:41 +0530

    Revision : Doubly Linked List | LC 430, 707 : Design Linked List

--- 16_linkList/03_implementDoublyLL.cpp
+++ 16_linkList/03_implementDoublyLL.cpp
@@ -1,6 +1,35 @@
 #include<iostream>
 using namespace std;
 
+//Revision Day 86
+
+/*
+    LeetCode 430 : Flatten a Multilevel Doubly Linked List
+             707 : Design Linked List
+
+    Topic: Doubly Linked List (DLL):
+        A linked list where each node contains data, 
+        a pointer to the next node, and a pointer to the previous node, 
+        allowing traversal in both directions.
+
+    Implemented:                                        Time Complexity:
+    1. push_front() - Insert node at the beginning.         O(1)
+    2. push_back()  - Insert node at the end.               O(1)
+    3. pop_front()  - Delete node from the beginning.       O(1)
+    4. pop_back()   - Delete node from the end.             O(1)
+    5. print()      - Traverse and display the DLL.         O(n)
+
+    Key Concept:
+    Each node contains two pointers:
+    next -> points to the next node
+    prev -> points to the previous node
+
+    Important:
+    Always handle the empty list and single-node cases
+    while performing insertion/deletion.
+    
+*/    
+
 class Node{
 
 public:
@@ -65,13 +94,19 @@ public:
             return;
         }
         Node* temp = head;
-        head = head->next;
 
-        if(head != NULL){
-            head->prev = NULL;
-            temp->next = NULL;
-            delete temp;
+        if(head == tail){       // Only one node
+            head = tail = NULL;
         }
+        else{
+            head = head->next;
+            head->prev = NULL;
+
+            // temp->next = NULL;
+            // delete temp;
+        }
+
+        delete temp;
    }
 
    void pop_back(){
@@ -80,13 +115,19 @@ public:
             return;
         }
         Node*temp = tail;
-        tail = tail->prev;
 
-        if(tail != NULL){
-            tail->next = NULL;
-            temp->prev = NULL;
-            delete temp;
+        if(head == tail){       // Only one node
+            head = tail = NULL;
         }
+        else{
+            tail = tail->prev;
+            tail->next = NULL;
+
+            // temp->prev = NULL;
+            // delete temp;
+        }
+
+        delete temp;
    }
 };
 
