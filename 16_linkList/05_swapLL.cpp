995c657678074723b44275cdd7f91a40dcf0bf52
NipurnCoder <nipurn.builds@gmail.com>
2026-08-11 22:16:03 +0530

    Revision : Swap Nodes in Pairs | LC : 24, 143 : Reorder List

--- 16_linkList/05_swapLL.cpp
+++ 16_linkList/05_swapLL.cpp
@@ -3,48 +3,107 @@
 #include<list>
 using namespace std;
 
-// class Solution {
-// public:
-//     ListNode* swapPairs(ListNode* head) {
-//         if(head == NULL || head->next == NULL){
-//             return head;
-//         }
-//         ListNode* first = head; 
-//         ListNode* sec = head->next;
-//         ListNode* prev = NULL;
+//Revision Day 88
 
-//         while(first != NULL && sec != NULL){
-//             ListNode* third = sec->next;
-//             sec->next = first;
-//             first->next = third;
-        
-//             if(prev != NULL){
-//                 prev->next = sec;
-//             } else {
-//                 head = sec;
-//             }
+/*
+    LeetCode 24 : Swap Nodes in Pairs
+            143 : Reorder List
 
-//             prev = first;
-//             first = third;
+    Topic: Linked List - Swap Nodes in Pairs
 
-//             if(third != NULL){
-//                 sec = third->next;
-//             } else{
-//                 sec = NULL;
-//             }
-//         }
-//         return head;
-//     }
-// };
+    Approach:
+    1. Take two nodes at a time: first and sec.
+    2. Store the next pair using third.
+    3. Reverse the current pair by changing next pointers.
+    4. Use prev to connect the previous swapped pair.
+    5. Move first and sec to the next pair.
 
-void middle(){
-        Node* slow = head;
-        Node* fast = head;
+    Time Complexity: O(n)
+    Space Complexity: O(1)
 
-        while(fast != NULL && fast->next != NULL){
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        //return slow;
-        cout<<slow->data<<endl;
+    Key Point:
+    Swap nodes by changing pointers, not their values.
+    If the number of nodes is odd, the last node remains unchanged.
+*/    
+
+class ListNode{
+public:
+    int val;
+    ListNode* next;
+
+    ListNode(int data){
+        val = data;
+        next = NULL;
     }
+};
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+
+        //Base Case
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+
+        ListNode* first = head; 
+        ListNode* sec = head->next;
+        ListNode* prev = NULL;
+
+        while(first != NULL && sec != NULL){
+
+            //Swap The Pairs
+            ListNode* third = sec->next;
+            sec->next = first;
+            first->next = third;
+        
+            //Connect Previous Pairs
+            if(prev != NULL){
+                prev->next = sec;
+            } else {
+                head = sec;
+            }
+
+            //Move to Next Pairs
+            prev = first;
+            first = third;
+
+            if(third != NULL){
+                sec = third->next;
+            } else{
+                sec = NULL;
+            }
+        }
+        return head;
+    }
+};
+
+
+void print(ListNode* head){
+    while(head != NULL){
+        cout<<head->val<<" ";
+        head = head->next;
+    }
+    cout<<endl;
+}
+
+int main(){
+
+    //Create Linked List
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    cout<<"Original Linked List "<<endl;
+    print(head);
+
+    cout<<"After Swaping "<<endl;
+
+    Solution s;
+    head = s.swapPairs(head);
+    print(head);
+
+    return 0;
+}
