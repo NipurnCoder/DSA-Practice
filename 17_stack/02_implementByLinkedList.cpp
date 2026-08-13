76406e69cdcb54b323f846a390331242016b0ce4
NipurnCoder <nipurn.builds@gmail.com>
2026-08-13 20:03:48 +0530

    Revision : Implementation By LinkedList | LC 739 : Daily Temperature, 844 : Backspace String Compare

--- 17_stack/02_implementByLinkedList.cpp
+++ 17_stack/02_implementByLinkedList.cpp
@@ -3,6 +3,33 @@
 #include<list>
 using namespace std;
 
+//Revision Day 90 [13/08/2026]
+
+/*
+    LeetCode 739 : Daily Temperatures
+             844 : Backspace String Compare
+
+    Topic: Stack Implementation using STL List
+
+    Stack follows LIFO (Last In, First Out).
+
+    Implemented:
+    1. push()  -> Insert element at front       O(1)
+    2. pop()   -> Remove element from front     O(1)
+    3. top()   -> Get front element             O(1)
+    4. empty() -> Check whether stack is empty  O(1)
+
+    Key Point:
+    Using list's front as the stack top allows
+    push and pop operations in O(1) time.
+
+    Example:
+    push(11), push(22), push(33)
+    Output: 33 22 11
+
+    Space: O(n)
+*/    
+
 class Stack{
     list<int> ll;
 
