47083caa1169da4dec6f7c139473fcb9f8ff1b58
NipurnCoder <nipurn.builds@gmail.com>
2026-08-30 20:27:41 +0530

    Revision : Kth Smallest Element in BST | LC 230, 530 : Min abs(diff)

--- 20_BST/02_KthSmallest.cpp
+++ 20_BST/02_KthSmallest.cpp
@@ -1,23 +1,52 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+#include<iostream>
+using namespace std;
+
+//Revision Day 107 [30/08/2026]
+
+/*
+    LeetCode 230: Kth Smallest Element in a BST
+             530: Minimun Absolute Differencce in BST
+
+    Approach:
+    1. Use Inorder Traversal: Left → Root → Right.
+    2. Inorder traversal of a BST gives nodes in sorted order.
+    3. Maintain a counter to count visited nodes.
+    4. When count + 1 == k, the current node is the kth smallest.
+    5. Stop traversal immediately once the answer is found.
+
+    Key Idea:
+    BST + Inorder Traversal = Sorted Order
+    kth visited node = kth smallest element.
+
+    Time Complexity: O(H + k) approximately
+    Space Complexity: O(H), due to recursion stack
+
+    Important:
+    The counter must be initialized to 0.
+*/
+
+class TreeNode{
+public:
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int data){
+        val = data;
+        left = right = NULL;
+    }
+};
+  
 class Solution {
 public:
-    int preOrder;
+    int count = 0;
     int kthSmallest(TreeNode* root, int k) {        //COMPLETE 50 QUESTIONS ON LEET CODE
 
         if(root == NULL){                           //COMPLETE 100 DSA VIDEOS 
             return -1;
         }
 
+        //left
         if(root->left != NULL){
             int leftAns = kthSmallest(root->left, k);
             if(leftAns != -1){
@@ -25,12 +54,14 @@ public:
             }
         }
 
-        if(preOrder+1 == k){
+        //root
+        if(count+1 == k){
             return root->val;
         }
 
-        preOrder++;
+        count++;
 
+        //right
         if(root->right != NULL){
             int rightAns = kthSmallest(root->right, k);
             if(rightAns != -1){
@@ -40,3 +71,17 @@ public:
         return -1;
     }
 };
+
+int main(){
+
+    TreeNode* root =  new TreeNode(3);
+    root->left = new TreeNode(1);
+    root->right = new TreeNode(4);
+    root->left->right = new TreeNode(2);
+
+    Solution s;
+
+    cout<<s.kthSmallest(root, 2);
+
+    return 0;
+}
