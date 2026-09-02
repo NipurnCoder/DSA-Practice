37e724cb5328644719c3799d52594b559635a506
NipurnCoder <nipurn.builds@gmail.com>
2026-09-02 17:01:49 +0530

    Revision Day 110 : Find Predecessor & Successor | LC 687, 110, 669

--- 20_BST/05_predSucc.cpp
+++ 20_BST/05_predSucc.cpp
@@ -2,6 +2,37 @@
 #include<vector>
 using namespace std;
 
+//Revision Day 110 [2/09/2026]
+
+/*
+    LeetCode 687 : Longest Univalue Path
+             110 : Balanced Binary Tree
+             669 : Trim a Binary Search Tree
+
+    Topic: Find Predecessor & Successor in BST
+
+    Approach:
+        1. Use BST properties to search for the key.
+        2. If key < curr->data:
+            → curr can be a possible Successor.
+            → Move to the left subtree.
+        3. If key > curr->data:
+            → curr can be a possible Predecessor.
+            → Move to the right subtree.
+        4. If the key is found:
+            → Predecessor = rightmost node of left subtree.
+            → Successor = leftmost node of right subtree.
+        5. If predecessor/successor does not exist, return -1.
+
+    Complexity:
+        Time  : O(h), where h = height of BST
+        Space : O(1)
+
+    Key Point:
+        Inorder predecessor = largest value smaller than key.
+        Inorder successor   = smallest value greater than key.
+*/
+
 class Node{
 public:
     int data;
@@ -58,11 +89,13 @@ vector<int> getPredSucc(Node* root, int key){
                 succ = leftMostInRightSubtree(curr->right);
             }
             break;
-
         }
-        
     }
-    return {pred->data, succ->data};
+    //return {pred->data, succ->data};
+    return { 
+        pred ? pred->data : -1,
+        succ ? succ->data : -1
+    }; 
 }
 
 int main(){
@@ -76,7 +109,7 @@ int main(){
     root->right->left = new Node(7);
     root->right->right = new Node(9);
 
-    int key = 3;
+    int key = 7;
 
     vector<int> ans = getPredSucc(root, key);
     cout<<"Predecessor : "<<ans[0]<<endl;
